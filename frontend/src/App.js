7b328bb1cdaad47e3f69e30aac959c12700b5381
ChocoPic <60088300+ChocoPic@users.noreply.github.com>
2023-10-12 17:36:25 +0900

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+import {BrowserRouter, Route, Routes, Outlet} from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage.jsx';
+
+function BasicLayout(){
+  return (
+    <div className="App">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<BasicLayout/>}>
+          <Route index element={<MainPage/>}/>
+          <Route path='/product/:productId' element={<DetailPage/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/MainPage/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <section>
+        {/* Navbar */}
+        {/* Banner*/}
+        <div>
+            {/* 이미지, 화살표, 제목 */}
+        </div>
+        {/* Game */}
+        <div>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default MainPage
